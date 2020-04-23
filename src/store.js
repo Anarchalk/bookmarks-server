568805f6598bb42f96e5bc0f682bd669220ddc1e
Anar Chalk <annachak9@gmail.com>
2020-04-23 17:23:36 -0400

    added router, store, logger files

---
+++ src/store.js
@@ -0,0 +1,19 @@
+//GET request
+const bookmarks = [
+    {
+      id: 1,
+      title: "Google",
+      url: "https://www.google.com",
+      description: "find the movies",
+      rating: 5,
+    },
+    {
+      id: 2,
+      title: "Aol",
+      url: "https://www.aol.com",
+      description: "Favorite list",
+      rating: 3,
+    },
+  ];
+
+  module.exports = bookmarks
